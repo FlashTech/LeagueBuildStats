9e75472edde35fdbd3118bbb8afae77892a05df7
FlashTech <lyon.steven.a@gmail.com>
2015-03-24 19:42:16 -0600

    Created initial test file for ChampionDatacorrections

---
+++ LeagueBuildStatsTests/Classes/Champions/ChampionDataCorrectionsTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueBuildStats.Classes.Champions;
+using NUnit.Framework;
+namespace LeagueBuildStats.Classes.Champions.Tests
+{
+	[TestFixture()]
+	public class ChampionDataCorrectionsTests
+	{
+		[Test()]
+		public void RunCorrectionsTest()
+		{
+			Assert.Fail();
+		}
+
+		[Test()]
+		public void CheckIfVersionIsGreaterEqualTest()
+		{
+			Assert.Fail();
+		}
+
+		[Test()]
+		public void RunStatCorrectionsTest()
+		{
+			Assert.Fail();
+		}
+	}
+}
+
+namespace LeagueBuildStatsTests.Classes.Champions
+{
+	class ChampionDataCorrectionsTests
+	{
+	}
+}
